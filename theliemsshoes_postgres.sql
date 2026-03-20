ff5bbf5e8d980a671dda1426f5a4c1bc77d585aa
loctran312 <lommlay@gmail.com>
2026-03-20 14:28:17 +0700

    update db

--- theliemsshoes_postgres.sql
+++ theliemsshoes_postgres.sql
@@ -29,6 +29,12 @@ CREATE TABLE categories (
   description TEXT
 );
 
+CREATE TABLE product_type (
+  id SERIAL PRIMARY KEY,
+  category_name VARCHAR(100) NOT NULL,
+  description TEXT
+);
+
 CREATE TABLE products (
   id SERIAL PRIMARY KEY,
   category_id INTEGER,
