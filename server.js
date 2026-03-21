6dd35b50348f12ce4fada7a9ec70dee1ad35f3b8
loctran312 <lommlay@gmail.com>
2026-03-21 17:00:25 +0700

    update: cartRoutes in server

--- server.js
+++ server.js
@@ -30,12 +30,14 @@ const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 const productRoutes = require('./routes/products');
 const categoryRoutes = require('./routes/categories');
+const cartRoutes = require('./routes/cart');
 
 // ===== API ROUTES =====
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 app.use('/categories', categoryRoutes);
+app.use('/cart', cartRoutes);
 
 // ===== HEALTH CHECK =====
 app.get('/', (req, res) => {
